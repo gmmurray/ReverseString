276bc992a71eafd14c25b3723ac6df7f145e97c4
trinilime <trinilime123@live.com>
2018-11-07 16:29:27 -0500

    Add project files.

---
+++ ReverseString/ReverseString.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string inputString = null;
+            string result = null;
+            bool run = true;
+
+            WriteLine("Input string to be reversed. Type exit to end program.");
+            while (run)
+            {
+                inputString = ReadLine();
+
+                if (inputString == "exit" || inputString == "Exit")
+                {
+                    run = false;
+                    break;
+                }
+                else
+                {
+
+                    char[] inputArray = inputString.ToCharArray();
+
+                    result = rString(inputArray, 0, inputArray.Length - 1);
+                    WriteLine(result);
+
+                    WriteLine();
+                    ReadKey();
+                }
+            }
+        }
+
+
+        public static string rString(char [] array, int start, int end)
+        {
+            char temp;
+            string result = null;
+
+            while (start < end)
+            {
+                temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;
+                end--;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                result += array[i];
+            }
+            return result;
+        }
+    }
+}
